b310f789f0fe08e7d3f46e4963496973f65d2ee3
Elizabeth Warren <elizabeth.s.warren@gmail.com>
2023-07-07 16:55:31 -0500

    Add explore.py

---
+++ explore.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+ ##############################################   VISUALIZE     ##############################################
+
+def plot_variable_pairs(df):
+    
+    df = df.sample(n = 10000)
+    
+    cols = ['bedcount', 'bathcount', 'sqfeet', 'yearbuilt', 'taxamount']
+    
+    target = 'value'
+    
+    for col in cols:
+        
+        sns.lmplot(df, x = col, y = target)
+        
+    return sns.lmplot(df, x = col, y = target)
+
+
+
+def plot_categorical_and_continuous_vars(df, cat_var_col, con_var_col):
+    
+    df = df.sample(n=1000)
+    
+    fig, axs = plt.subplots(1,3, figsize=(18,8))
+    
+    sns.stripplot(ax=axs[0], x=cat_var_col, y=con_var_col, data=df)
+    axs[0].set_title('stripplot')
+    
+    sns.boxplot(ax=axs[1], x=cat_var_col, y=con_var_col, data=df)
+    axs[1].set_title('boxplot')
+    
+    sns.swarmplot(ax=axs[2], x=cat_var_col, y=con_var_col, data=df, s=1)
+    axs[2].set_title('swarmplot')
